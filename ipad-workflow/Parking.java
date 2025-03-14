3976aef01cbf9f35fb63ad71e53bb80c7054dc11
Giuseppe Antonino Cotroneo <peppelion99@hotmail.it>
2025-03-14 14:01:22 +0100

    aa

--- ipad-workflow/Parking.java
+++ ipad-workflow/Parking.java
@@ -1,7 +1,7 @@
 public interface Parking {
     int maxParkingSpaces(); // Capienza Massima del Parcheggio
     int freeParkingSpaces(); // Numero di Posti Liberi. -1 Se non disponibili.
-    int Ticket enter(Ticket ticket); // Ritorna un Ticket per l'uscita e occupa il primo posto disponibile
+    Ticket enter(Ticket ticket); // Ritorna un Ticket per l'uscita e occupa il primo posto disponibile
     void payment(Ticket ticket); // Calcola il costo e lo annota sul Ticket
     boolean exit(Ticket ticket); // Libera il posto occupato dal Ticket
 } // Parking
